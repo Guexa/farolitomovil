894b5ef93b89d778f47e0db5e099eb8ae8edb39c
IDGS801-21000381 <sunmookmp@gmail.com>
2024-07-21 23:51:53 -0600

    "Hola mundo con la hoja de almacen"

--- lib/almacen.dart
+++ lib/almacen.dart
@@ -1,2 +1,37 @@
 //Hola aqui viene el crud
-// 
+//
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hola Mundo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hola Mundo'),
+      ),
+      body: Center(
+        child: Text(
+          '¡Hola Mundo!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
